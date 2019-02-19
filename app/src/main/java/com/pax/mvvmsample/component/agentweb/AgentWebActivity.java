7d3dfc18461f22198a69e49b348682d2a8bd8f04
linyd <1083996704@qq.com>
2019-02-19 18:02:20 +0800

    1

--- app/src/main/java/com/pax/mvvmsample/component/agentweb/AgentWebActivity.java
+++ app/src/main/java/com/pax/mvvmsample/component/agentweb/AgentWebActivity.java
@@ -142,15 +142,6 @@ public class AgentWebActivity extends AppCompatActivity {
 
         SuperLikeLayout superLikeLayout = findViewById(R.id.super_like_layout);
         BitmapProvider.Provider provider = new BitmapProvider.Builder(this).build();
-//        BitmapProvider.Provider provider = new BitmapProvider.Builder(this)
-//                .setDrawableArray(new int[]{R.mipmap.emoji_1, R.mipmap.emoji_2, R.mipmap.emoji_3, R.mipmap.emoji_4, R.mipmap.emoji_5, R.mipmap.emoji_6,
-//                        R.mipmap.emoji_7, R.mipmap.emoji_8, R.mipmap.emoji_9, R.mipmap.emoji_10, R.mipmap.emoji_11, R.mipmap.emoji_12, R.mipmap.emoji_13,
-//                        R.mipmap.emoji_14, R.mipmap.emoji_15, R.mipmap.emoji_16, R.mipmap.emoji_17, R.mipmap.emoji_18, R.mipmap.emoji_19, R.mipmap.emoji_20})
-//                .setNumberDrawableArray(new int[]{R.mipmap.multi_digg_num_0, R.mipmap.multi_digg_num_1, R.mipmap.multi_digg_num_2, R.mipmap.multi_digg_num_3,
-//                        R.mipmap.multi_digg_num_4, R.mipmap.multi_digg_num_5, R.mipmap.multi_digg_num_6, R.mipmap.multi_digg_num_7,
-//                        R.mipmap.multi_digg_num_8, R.mipmap.multi_digg_num_9})
-//                .setLevelDrawableArray(new int[]{R.mipmap.multi_digg_word_level_1, R.mipmap.multi_digg_word_level_2, R.mipmap.multi_digg_word_level_3})
-//                .build();
         superLikeLayout.setProvider(provider);
         ivLike.setOnClickListener(new View.OnClickListener() {
             @Override
